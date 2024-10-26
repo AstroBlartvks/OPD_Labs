153e5ca430fd06ea9f462bcbee1a4a83795851b2
Astro <98228094+AstroBlartvks@users.noreply.github.com>
2024-10-26 16:26:22 +0300

    Add files via upload

--- 4_find_filter.bash
+++ 4_find_filter.bash
@@ -7,16 +7,17 @@ ls -lRSrog lab0/ 2>/dev/null | grep '^[dl-][rwx-]' | grep "ru" | head -n 2
 echo -e "\n"
 
 echo TASK_3:
-cat -n $(ls -r lab0/p* lab0/*/p* lab0/*/*/p* 2>/dev/null) 2>/dev/null
+cat -n $(ls lab0/p* lab0/*/p* lab0/*/*/p* 2>/dev/null) 2>/dev/null | sort -rk3
 echo -e "\n" 
 
-echo TASK_4
+echo TASK_4:
 ls -1p lab0/purugly5/ 2>/tmp/myself_errors | grep -v "/$" 2>/tmp/myself_errors
 echo -e "\n"
 
-echo TASK_5
-cat -n $(ls -pr lab0/*/*/* | grep "6$")
+echo TASK_5:
+cat $(ls lab0/*/*/* | grep "6$") | sort -r
+echo -e "\n"
 
-echo TASK_6
+echo TASK_6:
 cat -n lab0/simisage8 2>/dev/null | grep -v "ck"
 echo -e "\n"
